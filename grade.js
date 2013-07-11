a4fa8f7ee543dff03eedbcc5472a2e6106c505d8
Floriano Oliveira <floriano76@gmail.com>
2013-07-11 04:21:57 +0000

    Hw3 done

--- grade.js
+++ grade.js
@@ -3,20 +3,38 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio  = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-	console.log("%s does not exist. Exiting. ", instr);
-	process.exit(1); // http://node.js/api/process.html#process_process_exit_code
+	if(!restlerUrl(instr)) {
+	    console.log("%s does not exist. Exiting. ", instr);
+	    process.exit(1); // http://node.js/api/process.html#process_process_exit_code
+	}
     }
     return instr;
 };
 
+var restlerUrl = function(url) {
+    return rest.get(url).on('complete', function(result) {
+	if (result instanceof Error) {
+	    return null;
+	}
+	return result;
+    });
+    
+};
+
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    if(fs.existsSync(htmlfile)) {
+	htmlfile = fs.readFileSync(htmlfile);
+    } else {
+	htmlfile = restlerUrl(htmlfile)
+    }
+    return cheerio.load(htmlfile);
 };
 
 var loadChecks = function(checksfile) {
@@ -44,10 +62,18 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_path>', 'URL path', clone(assertFileExists))
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if(program.url) {
+	var checkJson = checkHtmlFile(program.url, program.checks);
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
+    fs.writeFileSync("checks.txt", outJson); 
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
+
